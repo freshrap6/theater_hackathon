513244644f2958da121cd5cb8b95c04f8664c36f
Douglas Jones <d.m.jones2@gmail.com>
2021-04-28 13:57:10 +0100

    Adding missing proptypes

--- src/Components/TimeTableRow.jsx
+++ src/Components/TimeTableRow.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TimeTableRow = ({ entry }) => {
   return (
     <tr><td>{entry.days}</td><td>{entry.times}</td></tr>
   );
 };
+
+TimeTableRow.propTypes = {
+  entry: PropTypes.exact({
+    days: PropTypes.string,
+    times: PropTypes.string,
+    _id: PropTypes.number
+  })
+}
 
 export default TimeTableRow;
